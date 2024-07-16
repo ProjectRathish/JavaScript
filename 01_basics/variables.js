13f18cac3cfb65f2908005c29e8b8a449771373e
ProjectRathish <k.rathish.rathish@gmail.com>
2024-07-16 06:58:26 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountName="Rathish"
+var accountCity="Manjeri"
+let=123456
+accountState="Kerala"
+accountPin=33333
+
+accountCity="Devayani"
+
+
+console.table([accountName, accountCity, accountPin, accountState])
